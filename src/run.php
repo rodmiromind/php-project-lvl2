7086dcf60234b19a02ea26e16d828986d16a6628
notebook <onetreearmy@gmail.com>
2019-11-07 00:08:42 +0300

    step 3

--- src/run.php
+++ src/run.php
@@ -4,37 +4,35 @@ namespace GenDiff\src\run;
 
 function run($firstFilePath, $secondFilePath)
 {
-    var_dump(realpath($secondFilePath));
-    $before = json_decode(file_get_contents("/home/piston/hexlet/project_2/php-project-lvl2/bin/before.json"), TRUE);
-    $after = json_decode(file_get_contents(realpath($secondFilePath)), TRUE);
+    $firstFile = json_decode(file_get_contents(realpath($firstFilePath)), true);
+    $secondFile = json_decode(file_get_contents(realpath($secondFilePath)), true);
     $result = [];
     
-    foreach($before as $key => $item) {
-        if (!array_key_exists($key, $after)) {
+    foreach ($firstFile as $key => $item) {
+        if (!array_key_exists($key, $secondFile)) {
             $result['- ' . $key] = $item;
-        } elseif ($before[$key] === $after[$key]) {
+        } elseif ($firstFile[$key] === $secondFile[$key]) {
             $result['  ' . $key] = $item;
         } else {
             $result['- ' . $key] = $item;
-            $result['+ ' . $key] = $after[$key];
+            $result['+ ' . $key] = $secondFile[$key];
         }
     }
     
-    foreach($a as $key => $item) {
+    foreach ($a as $key => $item) {
         if (!array_key_exists($key, $b)) {
-            $c['+ ' . $key] = $item; 
+            $result['+ ' . $key] = $item;
         }
     }
     
-    var_dump($before);
-    var_dump($after);
-    var_dump($result);
-    echo "{" . PHP_EOL;
-    foreach($result as $key => $item) {
+    $resultForPrint = "{\n";
+    foreach ($result as $key => $item) {
         if (is_bool($item)) {
             $item = $item ? 'true' : 'false';
         }
-        echo '  ' . $key , ": " , $item . PHP_EOL;
+        $resultForPrint .= "  $key: $item\n";
     }
-    echo "}";
+    $resultForPrint .= "}";
+    echo $resultForPrint;
+    return $resultForPrint;
 }
