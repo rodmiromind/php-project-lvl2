25cf13de4625b0e0e0c57988c088918214461683
notebook <onetreearmy@gmail.com>
2019-11-22 23:36:39 +0300

    step6

--- src/genDiff.php
+++ src/genDiff.php
@@ -3,37 +3,74 @@
 namespace Differ;
 
 use function Differ\parsers\fileParse;
+use Funct;
+
+// переписать эти 2 функции в одну
+
+// function diffFrom1to2($before, $after)
+// {
+//     foreach($before as $key => $value) {
+//         if (!is_array($value) && array_key_exists($key, $after)) {
+//           if ($value !== $after[$key]) {
+//             $result[$key] = [[$value, 'removed'], [$after[$key], 'added']];
+//           } else {
+//             $result[$key] = [$value, 'not changed'];
+//           }
+//         } elseif (array_key_exists($key, $after)) {
+//           $result[$key] = array_diff_assoc_recursive($value, $after[$key]);
+//         } else {
+//           $result[$key] = [$value, 'removed'];
+//         }
+//       }
+    
+//     return $result;
+// }
+
+// function diffFrom2to1($after, $before)
+// {
+//     foreach($after as $key => $value) {
+//         if (!array_key_exists($key, $before)) {
+//           $result[$key] = [$value, 'added'];
+//         } elseif (is_array($value) && array_key_exists($key, $before)) {
+//           $new_diff = array_diff_assoc_recursive2($value, $before[$key]);
+//           if($new_diff != FALSE) {
+//             $result[$key] = $new_diff;
+//           }
+          
+//         }
+//       }
+      
+//     return $result;
+// }
+
+function diff($firstFile, $secondFile)
+{
+  // var_dump($firstFile, $secondFile);
+
+  function array_keys_recursive($firstFile) {
+    foreach($firstFile as $key => $value) {
+        $result[] = $key;
+        if(is_array($value)){
+            $result = array_merge($result, array_keys_recursive($value));
+        }
+    }
+    return $result;
+  }
+  $keys = Funct\Collection\union(array_keys_recursive($firstFile), array_keys_recursive($secondFile));
+  var_dump($keys);
+}
+
 
 function genDiff($firstFilePath, $secondFilePath)
 {
     $firstFile = fileParse($firstFilePath);
     $secondFile = fileParse($secondFilePath);
-    $result = [];
     
-    foreach ($firstFile as $key => $item) {
-        if (!array_key_exists($key, $secondFile)) {
-            $result['- ' . $key] = $item;
-        } elseif ($firstFile[$key] === $secondFile[$key]) {
-            $result['  ' . $key] = $item;
-        } else {
-            $result['- ' . $key] = $item;
-            $result['+ ' . $key] = $secondFile[$key];
-        }
-    }
+    // $resultDiff1to2 = diffFrom1to2($firstFile, $secondFile);
+    // $resultDiff2to1 = diffFrom2to1($secondFile, $firstFile);
     
-    foreach ($secondFile as $key => $item) {
-        if (!array_key_exists($key, $firstFile)) {
-            $result['+ ' . $key] = $item;
-        }
-    }
     
-    $resultForPrint = "{\n";
-    foreach ($result as $key => $item) {
-        if (is_bool($item)) {
-            $item = $item ? 'true' : 'false';
-        }
-        $resultForPrint .= "  $key: $item\n";
-    }
-    $resultForPrint .= "}";
-    return $resultForPrint;
+    // $diffData = array_merge_recursive($resultDiff1to2, $resultDiff2to1);
+    $a = diff($firstFile, $secondFile);
+    // var_dump($a);
 }
